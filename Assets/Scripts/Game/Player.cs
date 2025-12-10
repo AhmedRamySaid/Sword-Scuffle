2339197bf40a1c68bd8ba978f05e78a75618d8a7
A.Ramy <ahmedramysaidelnaggar@gmail.com>
2025-12-10 18:55:25 +0200

    Now blocking and attacking works

--- Assets/Scripts/Game/Player.cs
+++ Assets/Scripts/Game/Player.cs
@@ -1,251 +1,264 @@
-﻿using Game;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Player : MonoBehaviour
+namespace Game
 {
-    [Header("Movement Settings")] [SerializeField]
-    private float mSpeed = 10.0f;
-
-    [SerializeField] private float mJumpForce = 10.0f;
-    [SerializeField] private float mRollForce = 25.0f;
-    [SerializeField] private float mFallGravityMultiplier = 1.5f;
-
-    private Animator mAnimator;
-    private Rigidbody2D mBody2d;
-    private Sensor_HeroKnight mGroundSensor;
-
-    private bool mIsWallSliding;
-    private bool mGrounded;
-    
-    private float mDelayToIdle;
-    private float mRollCurrentTime;
-    private readonly float mRollDuration = 8f / 14f; // ~0.57s
-
-    private PlayerData lastSentData;
-    private PlayerData currentData;
-    private Vector3 lastSendPos;
-    
-    public bool isPlayer = false;    
-    public void Initialize()
+    public class Player : MonoBehaviour
     {
-        lastSentData = new PlayerData();
-        currentData = new PlayerData();
-        mAnimator = GetComponent<Animator>();
-        mBody2d = GetComponent<Rigidbody2D>();
-        mGroundSensor = transform.Find("GroundSensor").GetComponent<Sensor_HeroKnight>();
-    }
+        [Header("Movement Settings")]
+        [SerializeField] private float mSpeed = 10.0f;
+        [SerializeField] private float mJumpForce = 10.0f;
+        [SerializeField] private float mRollForce = 25.0f;
+        [SerializeField] private float mFallGravityMultiplier = 1.5f;
 
-    private void Update()
-    {
-        HandleTimers();
-        HandleGrounded();
-        HandleInput();
-        ApplyVariableGravity();
-        UpdateAnimator();
-    }
+        private Animator mAnimator;
+        private Rigidbody2D mBody2d;
+        private Sensor_HeroKnight mGroundSensor;
 
-    private void HandleTimers()
-    {
-        currentData.mTimeSinceAttack += Time.deltaTime;
-
-        if (currentData.mRolling)
-        {
-            mRollCurrentTime += Time.deltaTime;
-            if (mRollCurrentTime >= mRollDuration)
-            {
-                currentData.mRolling = false;
-                mRollCurrentTime = 0f;
-            }
-        }
-    }
-
-    private void HandleGrounded()
-    {
-        bool sensorState = mGroundSensor.State();
-
-        if (!mGrounded && sensorState)
-        {
-            mGrounded = true;
-            mAnimator.SetBool("Grounded", true);
-        }
-        else if (mGrounded && !sensorState)
-        {
-            mGrounded = false;
-            mAnimator.SetBool("Grounded", false);
-        }
-    }
-
-    private void HandleInput()
-    {
-        if (!isPlayer) return;
-        float inputX = Input.GetAxisRaw("Horizontal");
-        float inputY = Input.GetAxisRaw("Vertical");
-
-        // Flip sprite
-        if (inputX > 0)
-        {
-            GetComponent<SpriteRenderer>().flipX = false;
-            currentData.mFacingDirection = 1;
-        }
-        else if (inputX < 0)
-        {
-            GetComponent<SpriteRenderer>().flipX = true;
-            currentData.mFacingDirection = -1;
-        }
-
-        if (inputY < 0 && mBody2d.velocity.y > 0)
-        {
-            mBody2d.velocity = new Vector2(mBody2d.velocity.x, 0);
-        }
-
-        // Movement (disabled while rolling)
-        if (!currentData.mRolling)
-            mBody2d.velocity = new Vector2(inputX * mSpeed, mBody2d.velocity.y);
-
-        // Roll
-        if (Input.GetKeyDown(KeyCode.LeftShift) && !currentData.mRolling && !mIsWallSliding)
-        {
-            StartRoll();
-        }
-
-        // Jump
-        else if (Input.GetKeyDown(KeyCode.Space) && mGrounded && !currentData.mRolling)
-        {
-            Jump();
-        }
-
-        // Attack
-        else if (Input.GetMouseButtonDown(0) && currentData.mTimeSinceAttack > 0.25f && !currentData.mRolling)
-        {
-            Attack();
-        }
-
-        // Block
-        else if (Input.GetMouseButtonDown(1) && !currentData.mRolling)
-        {
-            mAnimator.SetTrigger("Block");
-            mAnimator.SetBool("IdleBlock", true);
-        }
-        else if (Input.GetMouseButtonUp(1))
-        {
-            mAnimator.SetBool("IdleBlock", false);
-        }
-
-        // Hurt / Death
-        else if (Input.GetKeyDown(KeyCode.Q) && !currentData.mRolling)
-            mAnimator.SetTrigger("Hurt");
-        else if (Input.GetKeyDown(KeyCode.E) && !currentData.mRolling)
-        {
-            mAnimator.SetTrigger("Death");
-        }
-
-        // Run / Idle states
-        if (Mathf.Abs(inputX) > Mathf.Epsilon)
-        {
-            mDelayToIdle = 0.05f;
-            mAnimator.SetInteger("AnimState", 1);
-        }
-        else
-        {
-            mDelayToIdle -= Time.deltaTime;
-            if (mDelayToIdle < 0)
-                mAnimator.SetInteger("AnimState", 0);
-        }
-    }
-
-    private void StartRoll()
-    {
-        currentData.mRolling = true;
-        mRollCurrentTime = 0f;
-        mAnimator.SetTrigger("Roll");
-        mBody2d.velocity = new Vector2(currentData.mFacingDirection * mRollForce, mBody2d.velocity.y);
-    }
-
-    private void Jump()
-    {
-        mAnimator.SetTrigger("Jump");
-        mGrounded = false;
-        mAnimator.SetBool("Grounded", false);
-
-        // Zero out vertical velocity to make jumps consistent
-        mBody2d.velocity = new Vector2(mBody2d.velocity.x, 0f);
-
-        // Apply upward impulse force instead of setting velocity
-        mBody2d.AddForce(Vector2.up * mJumpForce, ForceMode2D.Impulse);
-
-        mGroundSensor.Disable(0.2f);
-    }
-
-    private void Attack()
-    {
-        currentData.mCurrentAttack++;
-        if (currentData.mCurrentAttack > 3) currentData.mCurrentAttack = 1;
-        if (currentData.mTimeSinceAttack > 1.0f) currentData.mCurrentAttack = 1;
-
-        mAnimator.SetTrigger("Attack" + currentData.mCurrentAttack);
-        currentData.mTimeSinceAttack = 0.0f;
-    }
-
-    private void ApplyVariableGravity()
-    {
-        if (!mGrounded)
-        {
-            if (mBody2d.velocity.y < 0)
-            {
-                // Apply stronger gravity when falling
-                mBody2d.velocity += Vector2.up * Physics2D.gravity.y * (mFallGravityMultiplier - 1) * Time.deltaTime;
-            }
-            else if (mBody2d.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
-            {
-                // If player released jump early, apply slightly more gravity for a short hop
-                mBody2d.velocity += Vector2.up * Physics2D.gravity.y * (1.2f - 1) * Time.deltaTime;
-            }
-        }
-    }
-
-    private void UpdateAnimator()
-    {
-        mAnimator.SetBool("WallSlide", mIsWallSliding);
-        mAnimator.SetFloat("AirSpeedY", mBody2d.velocity.y);
-    }
-
-    public PlayerData GetDeltaData()
-    {
-        currentData.xPos = mBody2d.position.x;
-        currentData.yPos = mBody2d.position.y;
+        private bool mIsWallSliding;
+        private bool mGrounded;
         
-        PlayerData deltaData = PlayerData.SubtractData(currentData, lastSentData);
-        lastSentData.CopyData(currentData);
-        return deltaData;
-    }
+        private float mDelayToIdle;
+        private float mRollCurrentTime;
+        private readonly float mRollDuration = 8f / 14f; // ~0.57s
 
-    public void ApplyRealData(PlayerData data)
-    {
-        // Update facing direction
-        if (data.mFacingDirection != 0 && data.mFacingDirection != currentData.mFacingDirection)
+        private PlayerData lastSentData;
+        private PlayerData currentData;
+
+        public bool isPlayer = false;
+
+        // Animator hashes
+        private static readonly int HashRoll = Animator.StringToHash("Roll");
+        private static readonly int HashAttack1 = Animator.StringToHash("Attack1");
+        private static readonly int HashAttack2 = Animator.StringToHash("Attack2");
+        private static readonly int HashAttack3 = Animator.StringToHash("Attack3");
+        private static readonly int HashBlock = Animator.StringToHash("Block");
+        private static readonly int HashJump = Animator.StringToHash("Jump");
+        private static readonly int HashHurt = Animator.StringToHash("Hurt");
+        private static readonly int HashDeath = Animator.StringToHash("Death");
+        private static readonly int HashWallSlide = Animator.StringToHash("WallSlide");
+        private static readonly int HashAirSpeedY = Animator.StringToHash("AirSpeedY");
+        private static readonly int HashGrounded = Animator.StringToHash("Grounded");
+        private static readonly int HashAnimState = Animator.StringToHash("AnimState");
+        private static readonly int HashIdleBlock = Animator.StringToHash("IdleBlock");
+
+        public void Initialize()
         {
-            currentData.mFacingDirection = data.mFacingDirection;
-            GetComponent<SpriteRenderer>().flipX = currentData.mFacingDirection < 0;
+            lastSentData = new PlayerData();
+            currentData = new PlayerData();
+            mAnimator = GetComponent<Animator>();
+            mBody2d = GetComponent<Rigidbody2D>();
+            mGroundSensor = transform.Find("GroundSensor").GetComponent<Sensor_HeroKnight>();
         }
 
-        // Handle rolling
-        if (data.mRolling && !currentData.mRolling)
+        private void Update()
         {
-            // Start rolling animation for remote player
-            mAnimator.SetTrigger("Roll");
+            HandleTimers();
+            HandleGrounded();
+            HandleInput();
+            ApplyVariableGravity();
+            UpdateAnimator();
         }
-        currentData.mRolling = data.mRolling;
 
-        // Update attack
-        if (data.isAttacking && !currentData.isAttacking)
+        private void HandleTimers()
         {
-            int attackNumber = data.mCurrentAttack;
-            mAnimator.SetTrigger("Attack" + attackNumber);
-        }
-        currentData.isAttacking = data.isAttacking;
-        currentData.mCurrentAttack = data.mCurrentAttack;
+            currentData.mTimeSinceAttack += Time.deltaTime;
 
-        // Update position (you can also LERP for smooth movement)
-        mBody2d.position = new Vector2(data.xPos, data.yPos);
+            if (currentData.mRolling)
+            {
+                mRollCurrentTime += Time.deltaTime;
+                if (mRollCurrentTime >= mRollDuration)
+                {
+                    currentData.mRolling = false;
+                    mRollCurrentTime = 0f;
+                }
+            }
+        }
+
+        private void HandleGrounded()
+        {
+            bool sensorState = mGroundSensor.State();
+            if (!mGrounded && sensorState)
+            {
+                mGrounded = true;
+                mAnimator.SetBool(HashGrounded, true);
+            }
+            else if (mGrounded && !sensorState)
+            {
+                mGrounded = false;
+                mAnimator.SetBool(HashGrounded, false);
+            }
+        }
+
+        private void HandleInput()
+        {
+            if (!isPlayer) return;
+
+            float inputX = Input.GetAxisRaw("Horizontal");
+            float inputY = Input.GetAxisRaw("Vertical");
+
+            // Flip sprite
+            if (inputX > 0)
+            {
+                GetComponent<SpriteRenderer>().flipX = false;
+                currentData.mFacingDirection = 1;
+            }
+            else if (inputX < 0)
+            {
+                GetComponent<SpriteRenderer>().flipX = true;
+                currentData.mFacingDirection = -1;
+            }
+
+            if (inputY < 0 && mBody2d.velocity.y > 0)
+                mBody2d.velocity = new Vector2(mBody2d.velocity.x, 0);
+
+            // Movement (disabled while rolling)
+            if (!currentData.mRolling)
+                mBody2d.velocity = new Vector2(inputX * mSpeed, mBody2d.velocity.y);
+
+            // Roll
+            if (Input.GetKeyDown(KeyCode.LeftShift) && !currentData.mRolling && !mIsWallSliding)
+                StartRoll();
+
+            // Jump
+            else if (Input.GetKeyDown(KeyCode.Space) && mGrounded && !currentData.mRolling)
+                Jump();
+
+            // Attack
+            else if (Input.GetMouseButtonDown(0) && currentData.mTimeSinceAttack > 0.25f && !currentData.mRolling)
+                Attack();
+
+            // Block
+            else if (Input.GetMouseButtonDown(1) && !currentData.mRolling)
+            {
+                currentData.isBlocking = true;
+                mAnimator.SetTrigger(HashBlock);
+                mAnimator.SetBool(HashIdleBlock, true);
+            }
+            else if (Input.GetMouseButtonUp(1))
+            {
+                currentData.isBlocking = false;
+                mAnimator.SetBool(HashIdleBlock, false);
+            }
+
+            // Hurt / Death
+            else if (Input.GetKeyDown(KeyCode.Q) && !currentData.mRolling)
+                mAnimator.SetTrigger(HashHurt);
+            else if (Input.GetKeyDown(KeyCode.E) && !currentData.mRolling)
+                mAnimator.SetTrigger(HashDeath);
+
+            // Run / Idle
+            if (Mathf.Abs(inputX) > Mathf.Epsilon)
+            {
+                mDelayToIdle = 0.05f;
+                mAnimator.SetInteger(HashAnimState, 1);
+            }
+            else
+            {
+                mDelayToIdle -= Time.deltaTime;
+                if (mDelayToIdle < 0)
+                    mAnimator.SetInteger(HashAnimState, 0);
+            }
+        }
+
+        private void StartRoll()
+        {
+            currentData.mRolling = true;
+            mRollCurrentTime = 0f;
+            mAnimator.SetTrigger(HashRoll);
+            mBody2d.velocity = new Vector2(currentData.mFacingDirection * mRollForce, mBody2d.velocity.y);
+        }
+
+        private void Jump()
+        {
+            mAnimator.SetTrigger(HashJump);
+            mGrounded = false;
+            mAnimator.SetBool(HashGrounded, false);
+            mBody2d.velocity = new Vector2(mBody2d.velocity.x, 0f);
+            mBody2d.AddForce(Vector2.up * mJumpForce, ForceMode2D.Impulse);
+            mGroundSensor.Disable(0.2f);
+        }
+
+        private void Attack()
+        {
+            currentData.mCurrentAttack++;
+            if (currentData.mCurrentAttack > 3) currentData.mCurrentAttack = 1;
+            if (currentData.mTimeSinceAttack > 1.0f) currentData.mCurrentAttack = 1;
+
+            // Use cached hashes
+            int attackHash = currentData.mCurrentAttack switch
+            {
+                1 => HashAttack1,
+                2 => HashAttack2,
+                3 => HashAttack3,
+                _ => HashAttack1
+            };
+            mAnimator.SetTrigger(attackHash);
+            currentData.isAttacking = true;
+            currentData.mTimeSinceAttack = 0.0f;
+        }
+
+        private void ApplyVariableGravity()
+        {
+            if (!mGrounded)
+            {
+                if (mBody2d.velocity.y < 0)
+                    mBody2d.velocity += Vector2.up * Physics2D.gravity.y * (mFallGravityMultiplier - 1) * Time.deltaTime;
+                else if (mBody2d.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+                    mBody2d.velocity += Vector2.up * Physics2D.gravity.y * 0.2f * Time.deltaTime; // short hop
+            }
+        }
+
+        private void UpdateAnimator()
+        {
+            mAnimator.SetBool(HashWallSlide, mIsWallSliding);
+            mAnimator.SetFloat(HashAirSpeedY, mBody2d.velocity.y);
+        }
+
+        public PlayerData GetDeltaData()
+        {
+            currentData.xPos = mBody2d.position.x;
+            currentData.yPos = mBody2d.position.y;
+            PlayerData deltaData = PlayerData.SubtractData(currentData, lastSentData);
+            lastSentData.CopyData(currentData);
+            return deltaData;
+        }
+
+        public void ApplyRealData(PlayerData data)
+        {
+            // Facing direction
+            if (data.mFacingDirection != 0 && data.mFacingDirection != currentData.mFacingDirection)
+            {
+                currentData.mFacingDirection = data.mFacingDirection;
+                GetComponent<SpriteRenderer>().flipX = currentData.mFacingDirection < 0;
+            }
+
+            // Rolling
+            if (data.mRolling && !currentData.mRolling)
+                mAnimator.SetTrigger(HashRoll);
+            currentData.mRolling = data.mRolling;
+
+            // Attacking
+            if (data.isAttacking && !currentData.isAttacking)
+            {
+                int attackHash = data.mCurrentAttack switch
+                {
+                    1 => HashAttack1,
+                    2 => HashAttack2,
+                    3 => HashAttack3,
+                    _ => HashAttack1
+                };
+                mAnimator.SetTrigger(attackHash);
+            }
+            currentData.isAttacking = data.isAttacking;
+            currentData.mCurrentAttack = data.mCurrentAttack;
+
+            // Blocking
+            if (data.isBlocking && !currentData.isBlocking)
+                mAnimator.SetTrigger(HashBlock);
+            currentData.isBlocking = data.isBlocking;
+
+            // Smooth remote movement
+            mBody2d.position = Vector2.Lerp(mBody2d.position, new Vector2(data.xPos, data.yPos), 0.5f);
+        }
     }
 }
--- Assets/Scripts/Game/PlayerData.cs
+++ Assets/Scripts/Game/PlayerData.cs
@@ -1,21 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 
 namespace Game
 {
     public class PlayerData
     {
+        // Core state
         public bool mRolling;
         public bool isAttacking;
         public int mFacingDirection;
         public int mCurrentAttack;
-        public float mTimeSinceAttack; //todo: implement
+        public bool isBlocking; // NEW: track blocking
+        public float mTimeSinceAttack;
         public float xPos;
         public float yPos;
 
         public bool rollingChanged;
         public bool attackingChanged;
+        public bool blockingChanged; // track block state changes
         public uint id;
 
         public PlayerData()
@@ -27,132 +31,95 @@ namespace Game
             yPos = 0;
         }
 
-        public void CopyData(PlayerData playerData)
+        public void CopyData(PlayerData pd)
         {
-            mRolling = playerData.mRolling;
-            mFacingDirection = playerData.mFacingDirection;
-            mCurrentAttack = playerData.mCurrentAttack;
-            xPos = playerData.xPos;
-            yPos = playerData.yPos;
+            mRolling = pd.mRolling;
+            isAttacking = pd.isAttacking;
+            isBlocking = pd.isBlocking;
+            mFacingDirection = pd.mFacingDirection;
+            mCurrentAttack = pd.mCurrentAttack;
+            xPos = pd.xPos;
+            yPos = pd.yPos;
         }
 
-        public void Add(PlayerData playerData)
+        public void Add(PlayerData pd)
         {
-            xPos += playerData.xPos;
-            yPos += playerData.yPos;
-            
-            if (playerData.rollingChanged) mRolling = playerData.mRolling;
-            if (playerData.attackingChanged) isAttacking = playerData.isAttacking;
-            if (playerData.mFacingDirection != 0) mFacingDirection = playerData.mFacingDirection;
+            xPos += pd.xPos;
+            yPos += pd.yPos;
+
+            if (pd.rollingChanged) mRolling = pd.mRolling;
+            if (pd.attackingChanged) isAttacking = pd.isAttacking;
+            if (pd.blockingChanged) isBlocking = pd.isBlocking;
+            if (pd.mFacingDirection != 0) mFacingDirection = pd.mFacingDirection;
         }
-        
+
         public static PlayerData SubtractData(PlayerData newData, PlayerData oldData)
         {
-            PlayerData deltaData = new PlayerData();
+            PlayerData delta = new PlayerData
+            {
+                rollingChanged = oldData.mRolling != newData.mRolling,
+                attackingChanged = oldData.isAttacking != newData.isAttacking,
+                blockingChanged = oldData.isBlocking != newData.isBlocking,
+                mRolling = newData.mRolling,
+                isAttacking = newData.isAttacking,
+                isBlocking = newData.isBlocking,
+                mFacingDirection = newData.mFacingDirection,
+                mCurrentAttack = newData.mCurrentAttack - oldData.mCurrentAttack,
+                xPos = newData.xPos - oldData.xPos,
+                yPos = newData.yPos - oldData.yPos
+            };
 
-            deltaData.rollingChanged = oldData.mRolling != newData.mRolling;
-            deltaData.attackingChanged = oldData.isAttacking != newData.isAttacking;
-            
-            if (oldData.mFacingDirection != newData.mFacingDirection) 
-                deltaData.mFacingDirection = 0;
-            else deltaData.mFacingDirection = newData.mFacingDirection;
-            
-            deltaData.mRolling = newData.mRolling;
-            deltaData.mFacingDirection = newData.mFacingDirection;
-            deltaData.isAttacking = newData.isAttacking;
-            
-            deltaData.mCurrentAttack = newData.mCurrentAttack - oldData.mCurrentAttack;
-            deltaData.xPos = newData.xPos - oldData.xPos;
-            deltaData.yPos = newData.yPos - oldData.yPos;
-            
-            return deltaData;
+            return delta;
         }
+
+        // Optimized decoder using dictionary
+        private static readonly Dictionary<string, Action<PlayerData, string>> KeyActions = new()
+        {
+            ["Rolling"] = (pd, val) => { pd.mRolling = bool.Parse(val); pd.rollingChanged = true; },
+            ["Atk"] = (pd, val) => { pd.isAttacking = bool.Parse(val); pd.attackingChanged = true; },
+            ["Block"] = (pd, val) => { pd.isBlocking = bool.Parse(val); pd.blockingChanged = true; },
+            ["Facing"] = (pd, val) => pd.mFacingDirection = int.Parse(val),
+            ["X"] = (pd, val) => pd.xPos = float.Parse(val, CultureInfo.InvariantCulture),
+            ["Y"] = (pd, val) => pd.yPos = float.Parse(val, CultureInfo.InvariantCulture)
+        };
 
         public static PlayerData DeltaDataDecoder(string data)
         {
-            PlayerData newData = new PlayerData();
-            
-            // Split the string into key:value pairs
-            string[] pairs = data.Split(',');
+            PlayerData pd = new PlayerData();
+            var pairs = data.Split(',', StringSplitOptions.RemoveEmptyEntries);
 
-            foreach (string pair in pairs)
+            foreach (var pair in pairs)
             {
-                string[] kv = pair.Split(':');
+                var kv = pair.Split(':');
                 if (kv.Length != 2) continue;
 
-                string key = kv[0];
-                string value = kv[1];
-
-                switch (key)
-                {
-                    case "Rolling":
-                        newData.mRolling = bool.Parse(value);
-                        newData.rollingChanged = true;
-                        break;
-                    case "Facing":
-                        newData.mFacingDirection = int.Parse(value);
-                        break;
-                    case "Atk":
-                        newData.isAttacking = bool.Parse(value);
-                        newData.attackingChanged = true;
-                        break;
-                    case "X":
-                        newData.xPos = float.Parse(value, System.Globalization.CultureInfo.InvariantCulture);
-                        break;
-                    case "Y":
-                        newData.yPos = float.Parse(value, System.Globalization.CultureInfo.InvariantCulture);
-                        break;
-                }
+                if (KeyActions.TryGetValue(kv[0], out var action))
+                    action(pd, kv[1]);
             }
 
-            return newData;
+            return pd;
         }
 
-        /*
-         * Form:
-         * Rolling:{boolean},
-         * Atk:{boolean},
-         * Facing:{int},
-         * X:{float},
-         * Y:{float}
-         *
-         * The line breaks are purely visual and are not present within the packet
-         */
         public string ToDeltaString()
         {
             StringBuilder sb = new StringBuilder();
 
-            if (rollingChanged)
-                sb.Append($"Rolling:{mRolling},");
-            if (attackingChanged)
-                sb.Append($"Atk:{isAttacking},");
-            if (mFacingDirection != 0)
-                sb.Append($"Facing:{mFacingDirection},");
-            if (xPos != 0)
-                sb.Append($"X:{xPos},");
-            if (yPos != 0)
-                sb.Append($"Y:{yPos},");
-            
-            // Remove the trailing comma if present
-            if (sb.Length > 0)
-                sb.Length--;
-            
+            if (rollingChanged) sb.Append($"Rolling:{mRolling},");
+            if (attackingChanged) sb.Append($"Atk:{isAttacking},");
+            if (blockingChanged) sb.Append($"Block:{isBlocking},");
+            if (mFacingDirection != 0) sb.Append($"Facing:{mFacingDirection},");
+            if (xPos != 0) sb.Append($"X:{xPos},");
+            if (yPos != 0) sb.Append($"Y:{yPos},");
+
+            if (sb.Length > 0) sb.Length--; // remove trailing comma
             return sb.ToString();
         }
 
         public string ToRealString()
         {
-            StringBuilder sb = new StringBuilder();
-            
-            sb.Append($"Rolling:{mRolling},");
-            sb.Append($"Atk:{isAttacking},");
-            sb.Append($"Facing:{mFacingDirection},");
-            sb.Append($"X:{xPos},");
-            sb.Append($"Y:{yPos}");
-            
-            return sb.ToString();
+            return $"Rolling:{mRolling},Atk:{isAttacking},Block:{isBlocking},Facing:{mFacingDirection},X:{xPos},Y:{yPos}";
         }
-
+        
         public static PlayerData[] ParseRealData(string input)
         {
             if (string.IsNullOrWhiteSpace(input))
@@ -192,23 +159,21 @@ namespace Game
                             case "Rolling":
                                 pd.mRolling = bool.Parse(value);
                                 break;
-
                             case "Atk":
                                 pd.isAttacking = bool.Parse(value);
                                 break;
-
+                            case "Block":
+                                pd.isBlocking = bool.Parse(value);
+                                break;
                             case "Facing":
                                 pd.mFacingDirection = int.Parse(value);
                                 break;
-
                             case "X":
-                                pd.xPos = float.Parse(value, System.Globalization.CultureInfo.InvariantCulture);
+                                pd.xPos = float.Parse(value, CultureInfo.InvariantCulture);
                                 break;
-
                             case "Y":
-                                pd.yPos = float.Parse(value, System.Globalization.CultureInfo.InvariantCulture);
+                                pd.yPos = float.Parse(value, CultureInfo.InvariantCulture);
                                 break;
-
                             default:
                                 throw new FormatException($"Unknown key '{key}' in block '{block}'.");
                         }
